85918b68e63edaeb3b31795bf8b2ec2bc5dec704
Brayden Featheran <featheran@cox.net>
2021-02-28 19:51:34 -0800

    first commit for part 1 of instagram app

--- Instagram/AppDelegate.swift
+++ Instagram/AppDelegate.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Parse
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -14,9 +15,16 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        let parseConfig = ParseClientConfiguration {
+                    $0.applicationId = "VDfPYPQ80Yr1ZzoZVlGmWQ7kbo1VMRNFxiMTCwz8" // <- UPDATE
+                    $0.clientKey = "ObcKrOk0cb6rG0jGHpX4Jaqd08jTYBeJjzqu9fDt" // <- UPDATE
+                    $0.server = "https://parseapi.back4app.com"
+            }
+            Parse.initialize(with: parseConfig)
         return true
     }
-
+    
+  
     // MARK: UISceneSession Lifecycle
 
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
